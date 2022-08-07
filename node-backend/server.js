32c8f8616fab308d97ec91ea086b09802d330e03
asjadanis <asjadanis128@gmail.com>
2022-08-07 18:14:48 -0500

    chore: add dev script

--- node-backend/server.js
+++ node-backend/server.js
@@ -8,5 +8,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
 });
